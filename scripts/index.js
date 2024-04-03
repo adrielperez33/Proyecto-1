0e06b664625255e64bc6ac5eab2ce324cb820bb0
adriel <adrielperez227@gmail.com>
2024-04-03 18:06:23 -0300

    tercer commit segundo dia completo

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+class Activity{
+    constructor(id, title, description, imgUrl){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
+}
+
+class Repository{
+    constructor(){
+    }
+
+    activities(){
+        this.array = [];
+    }
+
+    getAllActivities(){
+        return this.array;
+    }
+
+    crearActivity(id, title, description, imgUrl){
+        let actividad = new Activity(id, title, description, imgUrl)
+        this.array.push(actividad);
+    }
+
+    deleteActivity(id){
+        this.array = this.array.filter(elemento => elemento.id !== id);
+    }
+    // aqui se implemento la funcion flecha por sugerencia de legibilidad de chatgpt
+}

297d530a1994ac74c3aa50491751c733eca1b796
adriel <adrielperez227@gmail.com>
2024-04-04 02:31:39 -0300

    commit homework 3

--- scripts/index.js
+++ scripts/index.js
@@ -9,11 +9,12 @@ class Activity{
 
 class Repository{
     constructor(){
-    }
-
-    activities(){
         this.array = [];
     }
+
+    // activities(){
+    //     this.array = [];
+    // }
 
     getAllActivities(){
         return this.array;
@@ -29,3 +30,78 @@ class Repository{
     }
     // aqui se implemento la funcion flecha por sugerencia de legibilidad de chatgpt
 }
+
+const i = new Repository();
+
+// const {id, title, description, imgUrl} = propiedades
+
+function tarjetasHtml(propiedades) {
+
+    const {id, title, description, imgUrl} = propiedades
+
+    const titulo = document.createElement("h3");
+    titulo.innerHTML = title;
+
+    const descripcion = document.createElement("p");
+    descripcion.innerHTML = description;
+
+    const img = document.createElement("img");
+    img.src = imgUrl;
+
+    const card = document.createElement("div");
+    titulo.classList.add("tituloCard")
+    descripcion.classList.add("descripcionCard")
+    img.classList.add("imagenCard")
+    card.classList.add("divCards")
+    card.appendChild(titulo)
+    card.appendChild(descripcion)
+    card.appendChild(img)
+
+    return card;
+}
+
+function contenedorNuevo() {
+    
+    const contenedor = document.getElementById("contenedor") 
+    
+    contenedor.innerHTML = "";
+
+    const guardado = i.getAllActivities();
+
+    const cards = guardado.map((propiedades) => tarjetasHtml(propiedades))
+
+    cards.forEach((cartas) => contenedor.appendChild(cartas));
+}
+
+let k = 1;
+
+function inputs() {
+    const titulo = document.getElementById("titulo").value
+    const description = document.getElementById("descripcion").value
+    const imagen = document.getElementById("url").value
+
+    if (titulo != "") {
+        
+        if (description != "") {
+        
+            if (imagen != "") {
+                contenedorNuevo();
+                i.crearActivity(k,titulo,description,imagen)
+                k++;
+            }else{
+                alert("falta completar la imagen")
+            }
+        }else{
+            alert("falta completar la descripcion")
+        }
+    }else{
+        alert("falta completar el titulo")
+    }
+}
+
+const boton = document.getElementsByClassName("glowing-btn")[0];
+
+boton.addEventListener("click",()=> {
+    // evento.preventDefault();
+    inputs();
+})
